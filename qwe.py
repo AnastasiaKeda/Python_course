739ecfaffc523e8cabc93ab469fac94cb5b0a582
Nastya <kednas.ananas@gmail.com>
2024-10-31 21:55:16 +0300

    test commit

---
+++ qwe.py
@@ -0,0 +1,3 @@
+
+a = list(filter(lambda x: len(x)>5,['ijfs', 'dii','sififjef']))
+print(a)
--- qwerty.py
+++ qwerty.py
@@ -1,1 +1,12 @@
-print('hi!')
+
+def int_pr(num):
+    if num < 10:
+        return num + 5
+    if num > 10:
+        return num - 3
+
+
+
+
+print(int_pr(100))
+
